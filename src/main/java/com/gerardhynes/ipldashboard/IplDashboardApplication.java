0834a6382eccb5d97dadb65865479b39e9fd57e6
Gerard Hynes <gkhynes@gmail.com>
2022-01-20 22:28:59 +0000

    Initial setup

---
+++ src/main/java/com/gerardhynes/ipldashboard/IplDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.gerardhynes.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+
+}
